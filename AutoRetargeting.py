4357a80498930bfaaf112f5ed37e9b68a74adae8
Guema <floorent1993@gmail.com>
2015-12-10 08:49:33 +0100

    wip

--- AutoRetargeting.py
+++ AutoRetargeting.py
@@ -1,7 +1,23 @@
 import bpy
 
-def normlize_BVH():
+
+def normlize_BVH(animation):
     """ this function make the bvh suitable for riggify"""
 
-    bpy.ops.object.rotation_euler[2] = -0.785398
+    print(animation)
+    bpy.context.object.rotation_euler[2] = -0.785398
+    return
+
+
+def retarget(rig, animation):
+    """ this function retarget context animation on rig"""
     bpy.context.space_data.context = 'OBJECT'
+    bpy.data.armature[]
+    return
+
+if len(bpy.context.selected_objects) == 1:
+    BvhAnimation = bpy.context.selected_objects[0]
+    Rig = bpy.ops.object.armature_human_metarig_add()
+
+normlize_BVH(BvhAnimation)
+retarget(Rig, BvhAnimation)
